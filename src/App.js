a0c6ac9dd2b274cf8874b48917a617abe272f1bf
copek4033 <cvyan403@gmail.com>
2020-11-13 18:32:52 +0700

    a

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React from "react";
+import styled from "styled-components";
+import TicTacToe from "./TicTacToe";
+import "papercss/dist/paper.min.css";
+
+function App() {
+  return (
+    <>
+      <Main>
+        <TicTacToe />
+      </Main>
+      <Footer>
+        <FooterInner>
+          Nama : Muhammad Thareeq Devyanto Ismail
+          Nim : 1119101734 {" "}
+          <a href="https://github.com/copek4033/mygame/">Github</a>
+        </FooterInner>
+      </Footer>
+    </>
+  );
+}
+
+const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  flex: 1 0 auto;
+`;
+
+const Footer = styled.footer`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  flex: 0 0 auto;
+`;
+
+const FooterInner = styled.div`
+  padding: 16px 0;
+`;
+export default App;
